701c73104de1c2104d9dc7825d422d74732b9c22
Cunnjoel <cunningham.joel84@gmail.com>
2021-03-18 20:10:06 -0500

    updating js

--- Main/script.js
+++ Main/script.js
@@ -3,5 +3,15 @@ console.log("hello world");
 
 
 $(document).ready(function(){
-    alert("jQuery ready")
+    var searchhistoryContainer = $("#past-searches");
+    var searchBtn = $("#search-btn");
+    var apiKey = "f7c1de4bc14295c86f377a66ad57e67d"
+
+    searchBtn.submit(function( event ) {
+        event.preventDefault();
+        console.log(event)
+        var formValues = $(this).serializeArray();
+        var city = formValues[0].value;
+        console.log(formValues, city)
+      });
 })
